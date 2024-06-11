978fdeced0f8b366cd8010dd31381fa7a7263280
Carterman555 <25chart555@gmail.com>
2024-06-11 07:51:31 -0600

    Add animations, work on waves

--- Assets/_Scripts/Managers/EnemyWaveManager.cs
+++ Assets/_Scripts/Managers/EnemyWaveManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace IslandDefender {
@@ -7,6 +8,8 @@ namespace IslandDefender {
 	/// in based on the difficultly.
 	/// </summary>
 	public class EnemyWaveManager : MonoBehaviour {
+
+		[SerializeField] private AnimationCurve spawnIntervalCurve;
 
 		private float currentDifficulty;
 		private int currentWave = 1;
@@ -22,5 +25,39 @@ namespace IslandDefender {
 			float difficultyIncrease = 5;
 			currentDifficulty += difficultyIncrease;
         }
+
+		private IEnumerator SpawnWave() {
+
+			float difficultyValueRemaining = currentDifficulty;
+
+			while (difficultyValueRemaining > 0) {
+
+				EnemyType enemyToSpawn = ChoseRandomEnemy(currentDifficulty, difficultyValueRemaining);
+				SpawnEnemy(enemyToSpawn);
+				
+				int strengthOfSpawned = -1;
+                difficultyValueRemaining -= strengthOfSpawned;
+
+                float interval = GetSpawnInterval(currentDifficulty);
+                yield return new WaitForSeconds(interval);
+			}
+		}
+
+		private float GetSpawnInterval(float difficulty) {
+            float avgInterval = spawnIntervalCurve.Evaluate(difficulty); // techinically not exactly avg, but close
+            float intervalVariance = 0.3f;
+            float interval = Random.Range(avgInterval * (1 - intervalVariance), avgInterval * (1 + intervalVariance));
+			return interval;
+        }
+
+        // chose random enemy weighted based on level difficulty and time in wave (later in wave gets harder)
+        private EnemyType ChoseRandomEnemy(float waveDifficulty, float difficultyValueRemaining) {
+			return EnemyType.Frog;
+		}
+
+		// probably need resource system
+		private void SpawnEnemy(EnemyType enemy) {
+
+		}
     }
 }
